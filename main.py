8147e6153239f80a5be693b602f4cb7dd4ed682f
MiguelOva <93915495+MiguelOva@users.noreply.github.com>
2022-04-17 21:22:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,5 @@ app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheet
                 meta_tags=[{'name': 'viewport',
                             'content': 'width=device-width, initial-scale=1.0'}]
                 )
+
+server = app.server
